e7a0037b0d08bae34c0055b2b708b9b5b9427fb9
krishna nand <65270325+krishna9786@users.noreply.github.com>
2023-07-02 22:49:39 +0530

    Create 2761. Prime Pairs With Target Sum.cpp

---
+++ Weekly Contest 352/2761. Prime Pairs With Target Sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> findPrimePairs(int n) {
+        //first calculate the prime number
+       vector <bool> prime(n+1, 1);
+        for(int i = 2; i <= n; i++) {
+            if(prime[i] == 0) continue;
+            for(int j = 2*i; j <= n; j+=i) {
+                prime[j] = 0;
+            }
+        }
+        
+        //iterate and find the pair whose sum is equal to n
+        vector<vector<int>> ans;
+        for(int i = 2; 2*i <= n; i++) {
+            if(prime[i] == 1 && prime[n-i] == 1) ans.push_back({i, n-i});
+        }
+        
+        return ans;
+    }
+};
